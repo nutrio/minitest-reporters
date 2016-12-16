c1bc958f079647f3995ffafbe0f82072b0990e2b
Chris Blackburn <chris@midwiretech.com>
2016-12-16 11:46:38 -0600

    NUT-3192 use strftime

--- lib/minitest/reporters/nutrio_reporter.rb
+++ lib/minitest/reporters/nutrio_reporter.rb
@@ -7,23 +7,14 @@ module Minitest
       def on_report
         super
         status_line = "Finished tests in %s, %.1f tests/s, %.1f assertions/s." %
-          [ameliorated_time_string(total_time), count / total_time, assertions / total_time]
+          [
+            Time.at(total_time).utc.strftime("%Hh %Mm %Ss"),
+            count / total_time,
+            assertions / total_time
+          ]
 
         puts
         puts colored_for(suite_result, status_line)
-      end
-
-      private
-
-      def ameliorated_time_string(seconds)
-        hours = seconds / 3600
-        mins  = seconds / 60 % 60
-        secs  = seconds % 60
-        [
-          hours >= 1 ? ("%.0dh" % hours) : nil,
-          mins  >= 1 ? ("%.0dm" % mins) : nil,
-          "%.0fs" % secs
-        ].join(' ').strip
       end
     end
 
